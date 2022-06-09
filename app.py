eeb1af6cdce49035e44efe6c9a6ecab6abf07710
Eve Morgan <evemorgan27@gmail.com>
2022-06-08 18:07:14 -0700

    finished flask madlibs

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, request
-#from flask_debugtoolbar import DebugToolbarExtension
+from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
-#debug = DebugToolbarExtension(app)
+debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def homepage():
-    prompts = request.args.get(silly_story.prompts)
+    prompts = silly_story.prompts
     #input_words = sample(#words list, number?)
 
-    return render_template('questions.html', noun = prompts)
+    return render_template('questions.html', prompts=prompts)
+
+
+@app.post('/story')
+def results():
+    results = request.form
+    result_story = silly_story.generate(results)
+
+    return render_template("story.html", story_text=result_story)
+    # return f'<h1>"{prompts}"</h1> '
+
